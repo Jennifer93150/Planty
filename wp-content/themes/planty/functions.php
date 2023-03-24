310f6ccc35274993f9eda583176adf5c00f33b29
Jennifer93150 <73002210+Jennifer93150@users.noreply.github.com>
2023-03-24 13:26:19 +0100

    creation of pages

---
+++ wp-content/themes/planty/functions.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Planty Theme functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package Planty
+ * @since 1.0.0
+ */
+
+/**
+ * Define Constants
+ */
+define( 'CHILD_THEME_PLANTY_VERSION', '1.0.0' );
+
+/**
+ * Enqueue styles
+ */
+function child_enqueue_styles() {
+
+	wp_enqueue_style( 'planty-theme-css', get_stylesheet_directory_uri() . '/style.css', array('astra-theme-css'), CHILD_THEME_PLANTY_VERSION, 'all' );
+	// Chargement du style.css du thème parent Twenty Twenty
+    wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+    // Chargement du css/theme.css pour nos personnalisations
+    wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/css/theme.css'));
+}
+
+add_action( 'wp_enqueue_scripts', 'child_enqueue_styles', 15 );
+
+
